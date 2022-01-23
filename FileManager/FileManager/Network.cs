40fbf0a85e2d040f211bfda67b5555ffafb6bfde
silenceafter <a59-info@yandex.ru>
2022-01-24 01:16:47 +0300

    FileManager

---
+++ FileManager/FileManager/Network.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    public class Network
+    {
+        public Network(string Name, FileSystem FileSystem) 
+        {
+            _Name = Name;
+            _Parent = FileSystem;
+            this.Computers = new List<Computer>();
+        }
+
+        private string _Name;
+        private FileSystem _Parent;
+        public List<Computer> Computers { get; }
+
+        public  bool AddComputer(Computer Computer)
+        {
+            return true;
+        }
+
+        public bool RemoveComputer(int ComputerId)
+        {
+            return true;
+        }
+
+        public Computer GetElement(int ComputerId)
+        {
+            return null;
+        }
+
+        public List<Computer> GetElementAll()
+        {
+            return new List<Computer>();
+        }
+
+        public FileSystem GetParent()
+        {
+            return null;
+        }
+    }
+}
---
+++ FileManager/FileManager/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    public class User
+    {
+        public User(string Name)
+        {
+            _Name = Name;
+            this.FolderByDefault = GetFolderByDefault();
+            this.Computer = Create();
+        }
+
+        private string _Name;
+        public string FolderByDefault { get; private set; }
+        public Computer Computer { get; }
+
+        public Computer Create()
+        {
+            //создать объект компьютера пользователя
+            return null;
+        }
+
+        public string GetFolderByDefault()
+        {
+            //вернуть папку, из которой запускается программа (либо другая, по умолчанию)
+            return this.FolderByDefault;
+        }
+    }
+}
