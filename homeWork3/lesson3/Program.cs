998b45cb5194a496972d8a717e824acc1bb5451e
silenceafter <a59-info@yandex.ru>
2022-02-08 00:59:35 +0300

    Задание 1-3 Обновление 08.02.22

--- homeWork3/lesson3/Program.cs
+++ homeWork3/lesson3/Program.cs
@@ -1,23 +1,111 @@
-﻿//задание 3
+﻿using System.IO;
+
+//задание 3
 var textFile = new TextFile("sample.txt");
+if (textFile != null)
+{
+    textFile.Read();
+    Console.WriteLine($"Файл { textFile.InputFile } создан");
+    Console.WriteLine("Чтение файла");
+    textFile.Search();
+    Console.WriteLine("Поиск e-mail адресов в тексте");
+    textFile.Write();
+    Console.WriteLine($"Запись файла { textFile.EmailFile }");
+    Console.WriteLine("Готово");
+}
 Console.ReadKey();
 
 public class TextFile
 {
     public TextFile(string filename) 
     {
-        Filename = filename;
+        InputFile = filename;
+        EmailFile = "email.txt";
+        FileByRows = new List<string>();
     }
 
-    private string _filename = "";
-    public string Filename
-    {
-        get => _filename;
-        set => _filename = value;
+    public List<string> FileByRows { get; set; }
+    public List<string> NewFileByRows { get; set; }
+    public string InputFile { get; private set; }
+    public string EmailFile { get; private set; }
+    public FileStream MyTextFile { get; private set; }
+    public FileStream Email { get; private set; }
+
+    public void Read()
+    {  
+        using var fstream = File.OpenRead(InputFile);
+        if (fstream != null)
+        {
+            byte[] array = new byte[fstream.Length];//преобразуем строку в байты            
+            fstream.Read(array, 0, array.Length);//считываем данные            
+            string text = System.Text.Encoding.Default.GetString(array);//декодируем байты в строку
+                                                                        //
+            string[] rows = text.Split('&');
+            for (var i = 0; i < rows.Length; i++)
+            {
+                string s = rows[i];
+                FileByRows.Add(rows[i]);
+            }
+        }
+        //ВОПРОС: Если при чтении возникнет ошибка, как лучше поступить с обработкой ошибки, учитывая, что у нас есть using
     }
 
-    public string SearchMail(ref string s)
+    public void Search()
     {
-        return "";
+        NewFileByRows = new List<string>();
+        for (int i = 0; i < this.FileByRows.Count; i++)
+        {
+            //выделим email из строки
+            var s = this.FileByRows[i];
+            this.SearchMail(ref s);
+            NewFileByRows.Add(s);
+        }
+    }
+
+    public void SearchMail(ref string s)
+    {
+        //выделить из строки адрес почты
+        string[] tmpArray = s.Split(' ');
+        //
+        foreach(string tmp in tmpArray)
+        {
+            if (tmp.Contains('@'))
+            {
+                //email обнаружен
+                s = tmp;
+                break;
+            }
+        }
+    }
+
+    public void Write()
+    {
+        //проверим существование файла
+        if (File.Exists(EmailFile))
+        {
+            File.Delete(EmailFile);
+        }
+
+        for (int i = 0; i < NewFileByRows.Count; i++)
+        {
+            var s = NewFileByRows[i];
+            if (s.Contains('@'))
+            {
+                WriteEmail(ref s);
+            }                
+        }
+    }
+
+    public void WriteEmail(ref string s)
+    {
+        // запись в файл
+        using var fstream = new FileStream(EmailFile, FileMode.Append);
+        if (fstream != null)
+        {
+            // преобразуем строку в байты
+            byte[] array = System.Text.Encoding.Default.GetBytes($"{ s }\n");
+            // запись массива байтов в файл
+            fstream.Write(array, 0, array.Length);
+        }
     }
 }
