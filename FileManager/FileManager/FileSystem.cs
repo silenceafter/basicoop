efd150dc9999858abebb5fb562d96a054da1b040
silenceafter <a59-info@yandex.ru>
2022-02-17 18:44:56 +0300

    FileManager

---
+++ FileManager/FileManager/FileSystem.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    public class FileSystem
+    {  
+        public FileSystem(
+            string Name, 
+            Computer Computer,
+            Network Network
+            )
+        {
+            _Name = Name;
+            _Parent = null;
+            this.Computer = Computer;
+            this.Network = Network;
+            var myObject = Create();
+        }
+
+        private string _Name;
+        private FileSystem _Parent;
+        public Computer Computer { get; }//ссылка на компьютер пользователя
+        public Network Network { get; }//ссылка на объект "Сеть"
+
+        public FileSystem Create()
+        {
+            //создать бинарное дерево
+            return null;
+        }
+
+        public bool Remove()
+        {
+            //удалить текущее дерево объектов
+            return true;
+        }
+
+        public bool AddComputer(Computer Computer)
+        {
+            return true;
+        }
+
+        public bool RemoveComputer(int ComputerId)
+        {
+            return true;
+        }
+
+        public bool AddNetwork(Network Network)
+        {
+            return true;
+        }
+
+        public bool RemoveNetwork(int NetworkId)
+        {
+            return true;
+        }
+        public FileSystem GetParent()
+        {
+            return null;
+        }
+    }
+}
---
+++ FileManager/FileManager/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FileManager;
+//
+//создаем объект User и Computer (через конструктор пользователя)
+var user = new User("User1");
+var system = new SystemOptions();//"системный" объект, с методами, который запускают основные действия программы
+var fileSystem = system.GetFileSystemTree();//создать дерево файловой системы
+
+//выводим на экран дерево файловой системы, показываем параметры файлов, папок, дисков и т.д.
